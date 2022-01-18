c2e29315d95239ae33cef39a86166617d5b94c9c
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-01-18 09:39:59 +0530

    feat: #express.js and handlerbar integration
    Created a simple web server using express.js and handlerbar. Configured express.js to respond to
    requests and serve web pages using handlerbar as a templating engine.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
 const path = require('path');
 const express = require('express');
-
-const publicDirPath = path.join(__dirname, './public');
-console.log(publicDirPath);
+const hbs = require('hbs');
 
 const app = express();
+
+// Set up directory paths for static assets and views
+const publicDirPath = path.join(__dirname, './public');
+const viewsDirPath = path.join(__dirname, '../templates/views');
+const partialsDirPath = path.join(__dirname, '../templates/partials');
+
+// Express settings for handlerbar views
+app.set('view engine', 'hbs');
+app.set('views', viewsDirPath);
+hbs.registerPartials(partialsDirPath);
+
+// Set up static directory to serve
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather app',
+    name: 'Avadhut',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Avadhut',
+  });
+});
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    name: 'Avadhut',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 'Help',
+    name: 'Avadhut',
+    errorMessage: 'Help article not found',
+  });
+});
+app.get('*', (req, res) => {
+  res.render('404', {
+    errorMessage: 'Page not found',
+  });
+});
 
 app.listen(3000, () => {
   console.log('\n\n----An express server listening on port 3000');
